c57e8c3da9590d299080b359a93e5805dbf9d4ae
Josy Santos <112010150+hijoosy@users.noreply.github.com>
2022-10-19 16:55:38 -0300

    Add files via upload

---
+++ ex4.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body><!--
+PEÇA PARA O ÚSUARIO DIGITAR O NOME E A NOTA FINAL DE 7 ALUNOS.
+EXIBA A MÉDIA DA TURMA E QUAIS ALUNOS POSSUEM NOTAS ACIMA DA MÉDIA E 
+QUAIS ESTÃO ABAIXO DA MÉDIA.-->
+    
+<form action="ex4.php" method="get">
+ Digite o nome do aluno:
+ <input type="text" name="nome"/>
+ <br/></br>
+ Digite a nota desse aluno:
+ <input type="number" name="nota"/>
+ <br/></br>
+ <input type="submit" value="Verificar"/>
+
+</form>
+
+
+
+
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
